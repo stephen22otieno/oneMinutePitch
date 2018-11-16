2160a4432a3b849d9ec9f5fc073892904b054227
stephen22otieno <stephenombiro22@gmail.com>
2018-11-16 12:52:09 +0300

    manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,35 @@
+from flask_script import Manager,Server
 from app import create_app,db
-from app.models import User
+from app.models import User, Role,Review
+from flask_migrate import Migrate, MigrateCommand 
+app = create_app('production')
+
+
+
+
+app = create_app('development')
+
+manager =Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+app = create_app('test')
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 @manager.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User )
+def make_shell_contex():
+    return dict(app=app, db=db, User=User, Role=Role)
+    
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
     manager.run()
