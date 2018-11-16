98cde824c25b8a4dafb1a8a6925df90aedae8e13
stephen22otieno <stephenombiro22@gmail.com>
2018-11-16 12:45:49 +0300

    Procfile and other modefied files

---
+++ config.py
@@ -0,0 +1,41 @@
+import os
+
+class Config:
+
+ 
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:stephen@localhost/One-minute-pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CND = True
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:stephen@localhost/One-minute-test_test'
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:stephen@localhost/One-minute-pitch'
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
+
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    
+    
---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='stephenotieno'
+export MAIL_USERNAME='Stephenombiro22@gmail.com '
+export MAIL_PASSWORD='Olive123'
+
+python3.6 manage.py server
